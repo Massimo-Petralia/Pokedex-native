92120fc2007fdebf4de0cf74b6c35078418dfbbc
Massimo <massipetra@hotmail.it>
2024-05-11 13:03:48 +0200

    unused code removed

--- state/pokemon.store.tsx
+++ state/pokemon.store.tsx
@@ -4,8 +4,5 @@ import {pokemonApi} from '../features/pokemon/services/pokemon.services';
 export const store = configureStore({
   reducer: {[pokemonApi.reducerPath]: pokemonApi.reducer},
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-    //  serializableCheck: false,
-      //immutableCheck: false,
-    }).concat(pokemonApi.middleware),
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
